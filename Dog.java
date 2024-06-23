52bab316afb7becfcd5f6fa5a73a108fd9a3b8ad
HeathenOne <121529768+HeathenOne@users.noreply.github.com>
2024-06-23 14:47:22 -0500

    Add files via upload

---
+++ Dog.java
@@ -0,0 +1,40 @@
+//Dylan Carter
+public class Dog extends RescueAnimal {
+
+    // Instance variable
+    private String breed;
+
+    // Constructor
+    public Dog(String name, String breed, String gender, String age,
+    String weight, String acquisitionDate, String acquisitionCountry,
+	String trainingStatus, boolean reserved, String inServiceCountry) {
+        setName(name);
+        setBreed(breed);
+        setGender(gender);
+        setAge(age);
+        setWeight(weight);
+        setAcquisitionDate(acquisitionDate);
+        setAcquisitionLocation(acquisitionCountry);
+        setTrainingStatus(trainingStatus);
+        setReserved(reserved);
+        setInServiceCountry(inServiceCountry);
+
+    }
+
+    // Accessor Method
+    public String getBreed() {
+        return breed;
+    }
+
+    // Mutator Method
+    public void setBreed(String dogBreed) {
+        breed = dogBreed;
+    }
+
+    //Format for Driver class
+    public String getFormattedDetails() {
+        String formattedDetails = String.format("Name: %s, Status: %s, Acquisition Country: %s, Reserved: %s",
+            getName(), getTrainingStatus(), getAcquisitionLocation(), (getReserved() ? "True" : "False"));
+        return formattedDetails;
+    }
+}
